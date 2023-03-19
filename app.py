bffa94021e79d52ec8a3aac267a89e4b7fec1338
obiifeka <obiifeka5@gmail.com>
2023-03-19 10:32:16 +0100

    second commit

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 from flask import Flask,redirect,url_for,render_template,request
 
 app=Flask(__name__)
-@app.route('/',methods=['GET','POST'])
-def home():
-    if request.method=='POST':
-        # Handle POST Request here
-        return render_template('index.html')
-    return render_template('index.html')
 
+jobs = [
+    {"id": 1, "title": "Software Engineer", "location": "San Francisco, CA", "salary": "$120,000 - $150,000 per year"},
+    {"id": 2, "title": "Data Analyst", "location": "New York, NY", "salary": "$80,000 - $100,000 per year"},
+    {"id": 3, "title": "Marketing Manager", "location": "Chicago, IL", "salary": "$90,000 - $110,000 per year"},
+    {"id": 4, "title": "Product Designer", "location": "Seattle, WA", "salary": "$100,000 - $130,000 per year"},
+    {"id": 5, "title": "Sales Executive", "location": "Los Angeles, CA", "salary": "$70,000 - $90,000 per year"}
+]
+
+
+@app.route('/')
+def home():
+        return render_template('index.html',jobs=jobs)
+    
+
+@app.route('/index')
+def index(): 
+        return render_template('index.html',jobs=jobs)
+   
+   
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
     app.run(port=5000,debug=True)
