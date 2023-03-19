f372fb7409ad6ff76685e5aed1559c158d37d586
obiifeka <obiifeka5@gmail.com>
2023-03-19 12:56:05 +0100

    latest commit, improved my landing page features

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,redirect,url_for,render_template,request
+from flask import Flask,redirect,url_for,render_template,request,jsonify
 
 app=Flask(__name__)
 
@@ -7,7 +7,8 @@ jobs = [
     {"id": 2, "title": "Data Analyst", "location": "New York, NY", "salary": "$80,000 - $100,000 per year"},
     {"id": 3, "title": "Marketing Manager", "location": "Chicago, IL", "salary": "$90,000 - $110,000 per year"},
     {"id": 4, "title": "Product Designer", "location": "Seattle, WA", "salary": "$100,000 - $130,000 per year"},
-    {"id": 5, "title": "Sales Executive", "location": "Los Angeles, CA", "salary": "$70,000 - $90,000 per year"}
+    {"id": 5, "title": "Sales Executive", "location": "Los Angeles, CA", "salary": "$70,000 - $90,000 per year"},
+     {"id": 6, "title": "Tecncal Support Assistant", "location": "Remote", "salary": ""}
 ]
 
 
@@ -17,8 +18,12 @@ def home():
     
 
 @app.route('/index')
-def index(): 
+def index():  
         return render_template('index.html',jobs=jobs)
+
+@app.route('/jobs')
+def list_jobs(): 
+        return jsonify(jobs)
    
    
 if __name__ == '__main__':
